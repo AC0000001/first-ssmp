169239b839d49cd546b46b85369a338e6f1271da
AC0000001 <1512395977@qq.com>
2021-11-25 17:24:27 +0800

    第一次修改

--- src/main/java/com/example/ssmp/controller/HelloController.java
+++ src/main/java/com/example/ssmp/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     public String Say(){
-        return "Hello N+";
+        return "Hello N+ ";
     }
 
 }
